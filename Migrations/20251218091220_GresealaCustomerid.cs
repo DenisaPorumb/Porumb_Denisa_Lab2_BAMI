25d18d3ad70d68a45b65e48632484470bdcafee9
DENISA-RALUCA PORUMB <denisa.porumb@stud.ubbcluj.ro>
2025-12-18 11:16:09 +0200

    greseala din cussomerId in customerID

---
+++ Migrations/20251218091220_GresealaCustomerid.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Porumb_Denisa_Lab2.Migrations
+{
+    /// <inheritdoc />
+    public partial class GresealaCustomerid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CusstomerID",
+                table: "Order");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CusstomerID",
+                table: "Order",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
--- Models/Order.cs
+++ Models/Order.cs
@@ -7,7 +7,7 @@ namespace Porumb_Denisa_Lab2.Models
     public class Order
     {
         public int OrderID { get; set; }
-        public int? CusstomerID { get; set; }
+        public int? CustomerID { get; set; }
         public int? BookID { get; set; }
 
         public DateTime OrderDate { get; set; }
